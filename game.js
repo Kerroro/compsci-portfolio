5ce64aa58b665e529162276df95ef39884a2dcd8
kerroro <rbevington.sh@mysummitps.org>
2016-03-23 22:13:11 +0000

    update

---
+++ game.js
@@ -0,0 +1,18 @@
+$('#target').click(function() {
+    $('#output').html(function(i, val) { return val*1+1 });
+});
+
+$('#cursorbutton').click(function() {
+    $('#cursors').html(function(i, val) { return val*1+1 });
+    $('#output').html(function(i,val) {
+        if(output>=10){
+            return val*1-10 if (output=)
+        }  
+        
+    });
+})
+
+
+
+
+//Functions above
---
+++ gif.js
@@ -0,0 +1,2 @@
+// JavaScript File
+<div id="sprite"></div>
